cd31d990f9c16a4721e0fd685fe97951edaf635d
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-02-24 21:55:04 +0530

    Update-LinkedList: Date_24-02-2023

---
+++ src/Theory/LinkedList/Singly_LL/LinkedList.java
@@ -0,0 +1,16 @@
+package Theory.LinkedList.Singly_LL;
+
+public class LinkedList {
+    Node head; // Head Attribute of the LinkedList Class:
+
+    // Nested Node Class:
+    private class Node{
+        int data;
+        Node next; // Default value of any object is "Null".
+
+        // Constructors
+        Node(int data){
+            this.data = data;
+        }
+    }
+}
