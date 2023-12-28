1e1dd5bb92d026f4820e3c71f203616dbcf3c649
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-12-28 22:11:54 +0530

    Very very bad way to accessing(read/write) the instance variable of the class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,18 @@
-import java.io.*;
-import java.util.*;
+class Keyboard {
+    int id;
+    String colour;
+    String connectivityType;
+    String keyType;
 
+}
 class Main{
-    public static void main(String args[]) {
-        System.out.println("Hello World!!");
+    public static void main(String[] args) {
+        System.out.println("OOPs");
+        Keyboard k1 = new Keyboard();
+        k1.colour = "Black";
+        k1.connectivityType = "Bluetooth";
+        k1.keyType = "Mechanical";
+        System.out.println("My Keyboard color is " + k1.colour + " and Type of " + k1.keyType + " and It is a " + k1.connectivityType + " keyboard.");
+
     }
 }
