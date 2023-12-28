f2b88e1a6526fa43f39d56a035cce60da9718679
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-12-28 21:46:52 +0530

    Initial Commit

--- src/Theory/OOPs/YouTube/OOPs_1.java
+++
@@ -1,9 +0,0 @@
-package Theory.OOPs.YouTube;
-
-public class OOPs_1 {
-    public static void main(String[] args) {
-        int a = 10;
-        Integer num = a;
-        System.out.println();
-    }
-}
