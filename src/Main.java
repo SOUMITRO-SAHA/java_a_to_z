61e208189ebee8b97e81bc0eaacc288a4f4db6c0
SOUMITRA-SAHA <soumitrosahaofficial@gmail.com>
2023-12-29 00:19:27 +0530

    Usage of 'this' keyword

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,87 @@
+// Explore Concepts through Commits: To delve into a specific topic, navigate to the corresponding commit.
+
+// OLD:
+//class Keyboard {
+//    int id;
+//    String colour;
+//    String connectivityType;
+//    String keyType;
+//
+//    // Constructor:
+//    // Default Constructor
+//    Keyboard() {
+//        this.id = 0;
+//        this.colour = "Black";
+//        this.connectivityType = "Bluetooth";
+//        this.keyType = "Mechanical";
+//    }
+//
+//    // Parameterized Constructor:
+//    // Customizes the Keyboard attributes based on user input
+//    // 2 Parameter
+//    public Keyboard(int id, String colour) {
+//        this.id = id;
+//        this.colour = colour;
+//    }
+//
+//    // 3 Parameter
+//    public Keyboard(int id, String colour, String connectivityType) {
+//        this.id = id;
+//        this.colour = colour;
+//        this.connectivityType = connectivityType;
+//    }
+//
+//    // 4 Parameter
+//    Keyboard(String colour, String connectivityType, String keyType) {
+//        this.colour = colour;
+//        this.connectivityType = connectivityType;
+//        this.keyType = keyType;
+//    }
+//
+//    // Copy Constructor:
+//    // Copy Constructor use to clone an object
+//    Keyboard(Keyboard k){
+//        this.id = k.id;
+//        this.colour = k.colour;
+//        this.connectivityType = k.colour;
+//        this.keyType = k.keyType;
+//    }
+//
+//    // Getter and Setter methods for encapsulation
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getColour() {
+//        return colour;
+//    }
+//
+//    public void setColour(String colour) {
+//        this.colour = colour;
+//    }
+//
+//    public String getConnectivityType() {
+//        return connectivityType;
+//    }
+//
+//    public void setConnectivityType(String connectivityType) {
+//        this.connectivityType = connectivityType;
+//    }
+//
+//    public String getKeyType() {
+//        return keyType;
+//    }
+//
+//    public void setKeyType(String keyType) {
+//        this.keyType = keyType;
+//    }
+//}
+
+// New: usage of "this" keyword:
 class Keyboard {
     int id;
     String colour;
@@ -15,19 +99,28 @@ class Keyboard {
 
     // Parameterized Constructor:
     // Customizes the Keyboard attributes based on user input
-    Keyboard(String colour, String connectivityType, String keyType) {
+    // 2 Parameter
+    public Keyboard(int id, String colour) {
+        this.id = id;
         this.colour = colour;
+    }
+
+    // 3 Parameter
+    public Keyboard(int id, String colour, String connectivityType) {
+        this(id, colour);
         this.connectivityType = connectivityType;
+    }
+
+    // 4 Parameter
+    Keyboard(int id, String colour, String connectivityType, String keyType) {
+        this(id, colour, connectivityType);
         this.keyType = keyType;
     }
 
     // Copy Constructor:
     // Copy Constructor use to clone an object
     Keyboard(Keyboard k){
-        this.id = k.id;
-        this.colour = k.colour;
-        this.connectivityType = k.colour;
-        this.keyType = k.keyType;
+        this(k.id, k.colour, k.connectivityType, k.keyType);
     }
 
     // Getter and Setter methods for encapsulation
@@ -66,29 +159,5 @@ class Keyboard {
 class Main{
     public static void main(String[] args) {
         System.out.println("OOPs");
-
-        // Creating a new Keyboard object using the default constructor
-        Keyboard k1 = new Keyboard();
-
-        // Setting the values using setter methods
-        k1.setId(1);
-        k1.setColour("Black");
-        k1.setConnectivityType("Bluetooth");
-        k1.setKeyType("Mechanical");
-
-        // Getting the Values:
-        System.out.println("My Keyboard 1: color is " + k1.getColour() + " and Type of " + k1.getKeyType()
-                + " and It is a " + k1.getConnectivityType() + " keyboard.");
-
-
-        // Copy Constructor
-        Keyboard kCopy = new Keyboard(k1);
-        System.out.println("My Keyboard Copy: color is " + kCopy.getColour() + " and Type of " + kCopy.getKeyType()
-                + " and It is a " + kCopy.getConnectivityType() + " keyboard.");
-
-        // O/P:
-        //OOPs
-        //My Keyboard 1: color is Black and Type of Mechanical and It is a Bluetooth keyboard.
-        //My Keyboard Copy: color is Black and Type of Mechanical and It is a Black keyboard.
     }
 }
